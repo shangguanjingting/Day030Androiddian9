432bf8f7415bc70115dcaab3a58c7e4a48d78f1d
shangguanjingting <1763468783@qq.com>
2016-10-23 17:42:40 +0800

    第二次上传

--- app/src/main/java/com/qm/administrator/day030androiddian9/MainActivity.java
+++ app/src/main/java/com/qm/administrator/day030androiddian9/MainActivity.java
@@ -1,5 +1,8 @@
 package com.qm.administrator.day030androiddian9;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Canvas canvas = new Canvas();
+        Paint paint = new Paint();
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP){
+            canvas.drawOval(100,200,300,400,paint);
+        }
     }
 }
